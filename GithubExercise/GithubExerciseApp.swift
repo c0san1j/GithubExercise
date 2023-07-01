6c2c2b62fbddfce25d4ce79cb5ca613b12f9e6ca
Joel Santiago <jsvalentin@gmail.com>
2023-07-01 17:42:12 -0400

    Initial Commit

---
+++ GithubExercise/GithubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubExerciseApp.swift
+//  GithubExercise
+//
+//  Created by Joel Santiago on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
