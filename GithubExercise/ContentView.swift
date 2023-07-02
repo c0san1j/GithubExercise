5f76f8c7fda32b100b5d57a8cf91872be57f5400
Joel Santiago <jsvalentin@gmail.com>
2023-07-02 14:52:10 -0400

    Version 5.0 - Added Button with State function

--- GithubExercise/ContentView.swift
+++ GithubExercise/ContentView.swift
@@ -8,22 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numberOfCircle = 0
     var body: some View {
-        VStack {
-            Image(systemName: "4.circle")
-                .resizable()
-                .scaledToFit()
-            Text("Version 4.0")
-                .font(.largeTitle)
-            Rectangle()
+        ZStack {
+            Color.purple
+                .ignoresSafeArea()
+            VStack {
+                
+                VStack {
+                    Image(systemName: "\(numberOfCircle).circle")
+                        .resizable()
+                        .scaledToFit()
+                    Text("Version \(numberOfCircle).0")
+                        .font(.largeTitle)
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color.black.opacity(0.2))
+                        .frame(width: 350, height: 100)
+                        .overlay(
+                            HStack {
+                                Spacer()
+                                
+                                Button {
+                                    if numberOfCircle < 50 {
+                                        numberOfCircle += 1
+                                    }
+                                } label: {
+                                    Text("Upgrade Version")
+                                }
+                                .frame(width: 100)
+                                .padding(.horizontal)
+                                .padding(.vertical, 10)
+                                .background(.blue)
+                                .cornerRadius(15)
+                                
+                                Spacer()
+                                
+                                Button {
+                                    if numberOfCircle > 0 {
+                                        numberOfCircle -= 1
+                                    }
+                                } label: {
+                                    Text("Downgrade Version")
+                                }
+                                .frame(width: 100)
+                                .padding(.horizontal)
+                                .padding(.vertical, 10)
+                                .background(.blue)
+                                .cornerRadius(15)
+                                
+                                Spacer()
+                            }
+                                .foregroundColor(.white)
+                                .tint(Color.blue)
+                        )
+                }
+                .foregroundColor(.white)
+                .padding()
+                
+                
+            }
+            .background(
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(Color.black.opacity(0.2))
+            )
+            .padding(.horizontal, 8)
         }
-        .foregroundColor(.purple)
-        .padding()
-        .background(
-            RoundedRectangle(cornerRadius: 5)
-                .fill(.ultraThinMaterial)
-        )
-        .padding(.horizontal, 8)
     }
 }
 
