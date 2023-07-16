ee0735aa9fc56d3717bdd8aaa40d3bb50b6a1474
Joel Santiago <jsvalentin@gmail.com>
2023-07-16 12:18:35 -0400

    development - added version 5.0

--- GithubExercise/ContentView.swift
+++ GithubExercise/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var numberOfCircle = 0
+    @State private var numberOfCircle = 5
     var body: some View {
         ZStack {
             Color.purple
@@ -43,6 +43,12 @@ struct ContentView: View {
                                 
                                 Spacer()
                                 
+                                Divider()
+                                    .background(.black.opacity(0.75))
+                                    .padding()
+                                
+                                Spacer()
+                                
                                 Button {
                                     if numberOfCircle > 0 {
                                         numberOfCircle -= 1
@@ -58,8 +64,6 @@ struct ContentView: View {
                                 
                                 Spacer()
                             }
-                                .foregroundColor(.white)
-                                .tint(Color.blue)
                         )
                 }
                 .foregroundColor(.white)
